111ab93781562e0729c2166c4563bc6f8003ae4b
jacobballard <jacob.ballard.1999@gmail.com>
2019-02-25 17:52:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,11 @@ var s = document.createElement("script");
 s.src="https://d3js.org/d3.v5.js";
 var body = document.getElementsByTagName("body")[0];
 body.after(s);
-
-d3.selectAll(".fusion-builder-row.fusion-row").selectAll("a").nodes();
-
-var L = [];
-var f = fuction(d){
-L.add(d);}
-
-
+//select the array where the text is stored.
 x = d3.selectAll(".fusion-builder-row.fusion-row").selectAll("a").nodes().slice(1)
+
+//Pull text from the lists.
 x.forEach(function(element) {
   console.log(element.text);
 });
+
